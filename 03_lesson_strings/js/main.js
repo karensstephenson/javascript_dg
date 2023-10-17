fc908ec5d11fa4bba71772713a90633dc6203270
Karen Stephenson <135253615+karensstephenson@users.noreply.github.com>
2023-10-17 12:50:58 +0100

    strings

---
+++ 03_lesson_strings/js/main.js
@@ -0,0 +1,29 @@
+//Strings
+const myVariable = "Mathematics";
+
+//The length property
+console.log(myVariable.length)
+
+console.log("Every good boy does fine".length)
+
+//String methods
+console.log(myVariable.charAt(0))
+console.log(myVariable.charAt(5))
+
+console.log(myVariable.indexOf("Mat"))
+console.log(myVariable.indexOf("mat"))
+
+console.log(myVariable.lastIndexOf("ath"))
+
+console.log(myVariable.slice(5,7))
+console.log(myVariable.slice(7))
+
+console.log(myVariable.toUpperCase())
+console.log(myVariable.toLowerCase())
+
+console.log(myVariable.includes('ath'))
+console.log(myVariable.includes('eth'))
+
+console.log(myVariable.split('a')) //'a' not included in the resulting strings
+
+
