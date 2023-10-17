b428741d8c10adacb879c37f4732bf820576bd6c
Karen Stephenson <135253615+karensstephenson@users.noreply.github.com>
2023-10-17 13:45:53 +0100

    math methods

---
+++ 05_lesson_math_methods/js/main.js
@@ -0,0 +1,26 @@
+// Math methods and properties
+
+console.log(Math.PI)
+console.log(Math.trunc(Math.PI))
+
+console.log(Math.round(Math.PI))
+
+console.log(Math.ceil(Math.PI))
+
+console.log(Math.floor(Math.PI))
+
+console.log(Math.pow(2, 4))
+
+console.log(Math.min(2, 3, 6))
+
+console.log(Math.max(4, 5, 7))
+
+console.log(Math.random())
+//From 1 to 10
+console.log(Math.floor(Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1)
+//From 2 to 8 (includes 7 numbers)
+console.log(Math.floor(Math.random()*7)+2)
+
+
