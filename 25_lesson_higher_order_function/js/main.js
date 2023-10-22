92930ef05f2ad078be2dcfb1cde3d79978892815
Karen Stephenson <135253615+karensstephenson@users.noreply.github.com>
2023-10-22 15:23:46 +0100

    higher order functions

---
+++ 25_lesson_higher_order_function/js/main.js
@@ -0,0 +1,35 @@
+// Higher Order Functions
+
+// A higher order function is a function that does at least one of the following:
+// ** Takes one or more functions as an argument (parameter)
+// ** Returns a function as the result
+import { posts } from "./posts.js"
+
+// forEach()
+posts.forEach(post => {
+    console.log(post)
+})
+console.clear()
+
+//filter()
+const filteredPosts = posts.filter(post => {
+    return post.userId === 1
+})
+console.log(filteredPosts)
+console.clear()
+
+// map()
+const mappedPosts = filteredPosts.map(post => {
+    return post.id * 10
+})
+console.log(mappedPosts)
+
+// reduce()
+const reducedPostsValue = mappedPosts.reduce((sum, post) => {
+    return sum + post
+})
+console.log(reducedPostsValue)
+
+
+
+
